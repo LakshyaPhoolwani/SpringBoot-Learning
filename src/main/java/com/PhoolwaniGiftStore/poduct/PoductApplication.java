dcb477870dbdafceec9a2279a2bb43857ae46be8
LakshyaPhoolwani <lakshyaphoolwani11102003@gmail.com>
2026-06-26 16:28:06 +0530

    Initial commit

---
+++ src/main/java/com/PhoolwaniGiftStore/poduct/PoductApplication.java
@@ -0,0 +1,13 @@
+package com.PhoolwaniGiftStore.poduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PhoolwaniGiftStore/poduct/PoductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PhoolwaniGiftStore.poduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
